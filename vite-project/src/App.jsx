24418307d266aec1e430fd2b86793df0dfa4197b
Pradyut20 <pradyutsahoo.2003@gmail.com>
2024-03-28 01:21:54 +0530

    Issue fix

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import WeatherApp from "../src/weatherApp/WeatherApp";
 
 const App = () => {
   return (
-    <>
-      <WeatherApp />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<WeatherApp />} />
+      </Routes>
+    </Router>
   );
 };
 
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client"; // Correct import for React 18+
+import { BrowserRouter } from "react-router-dom"; // Correct import statement
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
