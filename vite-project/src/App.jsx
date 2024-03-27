aa5d680748dedabfdc9fb80e886cdb7cf9ee5a59
Pradyut20 <pradyutsahoo.2003@gmail.com>
2024-03-28 01:05:57 +0530

    New Weather App

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WeatherApp from "../src/weatherApp/WeatherApp";
+
+const App = () => {
+  return (
+    <>
+      <WeatherApp />
+    </>
+  );
+};
+
+export default App;
