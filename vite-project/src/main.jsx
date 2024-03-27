4be870eaf410d97feab8481105c7734dcfadc973
Pradyut20 <pradyutsahoo.2003@gmail.com>
2024-03-28 01:34:16 +0530

    Small Fixes

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; // Correct import for React 18+
-import { BrowserRouter } from "react-router-dom"; // Correct import statement
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
